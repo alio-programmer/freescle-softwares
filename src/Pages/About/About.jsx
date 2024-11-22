0ee7416ee496f99ea2aab934374299bf77fa8045
Gaurav singh bisht <gauravsinghbisht3@gmail.com>
2024-11-22 15:37:20 +0530

    service page added

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,9 @@
+const About = () =>{
+    return (
+        <div className="grid grid-cols-2 ">
+            <h1>Hello world!</h1>
+        </div>
+    )
+}
+
+export default About;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () =>{
     return (
-        <div className=" w-full h-screen bg-gradient-to-b from-black to-violet-950 grid grid-cols-2 ">
+        <div className="grid md:grid-cols-2 gap-4 ">
 
         </div>
     )
